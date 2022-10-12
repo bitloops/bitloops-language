8065438f877c3bd53c13de2b7c80f2b8be4ddaad
ellik95 <ellikotoula@gmail.com>
2022-10-12 15:59:24 +0300

    added base fastify controller

--- boilerplate/ts/infra/rest/fastify/src/index.ts
+++ boilerplate/ts/infra/rest/fastify/src/index.ts
@@ -1,14 +1,17 @@
 import FastifyServer, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import formBodyPlugin from '@fastify/formbody';
 import fastifyCors from '@fastify/cors';
+import { BaseFastifyController } from './models/BaseFastifyController';
 
 namespace Fastify {
   export type Instance = FastifyInstance;
   export type Reply = FastifyReply;
   export type Request = FastifyRequest;
+  export const Server = FastifyServer;
 
+  export const BaseController = BaseFastifyController;
   export const formBody = formBodyPlugin;
   export const cors = fastifyCors;
 }
 
-export { Fastify, FastifyServer };
+export { Fastify };
