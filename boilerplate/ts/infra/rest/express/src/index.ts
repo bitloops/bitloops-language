053585a65ef6545c31bda22970a2642e7ef41b7a
ellik95 <ellikotoula@gmail.com>
2022-10-12 19:18:21 +0300

    fixed helmet import and made package support esm packages

--- boilerplate/ts/infra/rest/express/src/index.ts
+++ boilerplate/ts/infra/rest/express/src/index.ts
@@ -2,16 +2,15 @@ import express from 'express';
 import bodyParserImport from 'body-parser';
 import morganImport from 'morgan';
 import corsImport from 'cors';
-import helmetImport from 'helmet';
-import { BaseExpressController } from './models/BaseExpressController';
+import helmet from 'helmet';
+import { BaseExpressController } from './models/BaseExpressController.js';
 
 namespace Express {
   export const app = express;
   export const bodyParser = bodyParserImport;
   export const morgan = morganImport;
   export const cors = corsImport;
-  export const helmet = helmetImport;
   export const BaseController = BaseExpressController;
 }
 
-export { Express };
+export { Express, helmet };
