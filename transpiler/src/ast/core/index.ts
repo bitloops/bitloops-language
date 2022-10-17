dd3779dba6ed9bbf21cc428f7b82ce77f5f1e6da
Vasilis Danias <vasilios.danias@hec.edu>
2022-10-17 12:45:09 +0300

    Expression test and visitor vs parseBitloops implementation

--- transpiler/src/ast/core/index.ts
+++ transpiler/src/ast/core/index.ts
@@ -1,5 +1,6 @@
 import { BitloopsLanguageASTContext } from '../../index.js';
 import { TBoundedContexts } from '../../types.js';
+import { parseBitloops } from './BitloopsParser.js';
 import BitloopsVisitor from './BitloopsVisitor/BitloopsVisitor.js';
 
 export interface IBitloopsIntermediateASTParser {
@@ -8,10 +9,54 @@ export interface IBitloopsIntermediateASTParser {
 
 export class BitloopsIntermediateASTParserError extends Error {}
 
+const migratedTypes = ['jestTestDeclaration'];
+
 export class BitloopsIntermediateASTParser implements IBitloopsIntermediateASTParser {
   parse(ast: BitloopsLanguageASTContext): TBoundedContexts | BitloopsIntermediateASTParserError {
-    const bitloopsVisitor = new BitloopsVisitor();
-    bitloopsVisitor.visit(ast);
-    return bitloopsVisitor.result;
+    let boundedContextsData: TBoundedContexts = {};
+    let partialBoundedContextsData: TBoundedContexts = {};
+    for (const [boundedContextName, boundedContext] of Object.entries(ast)) {
+      for (const classes of Object.values(boundedContext)) {
+        for (const classData of Object.values(classes)) {
+          if (migratedTypes.includes(classData.deprecatedAST.type)) {
+            const bitloopsVisitor = new BitloopsVisitor();
+            const result = bitloopsVisitor.visitChildren(classData.initialAST)[0][0][2];
+            return result;
+          } else {
+            partialBoundedContextsData = parseBitloops(
+              boundedContextName,
+              classData.module,
+              classData.deprecatedAST,
+            );
+          }
+          boundedContextsData = mergeBoundedContextData(
+            boundedContextsData,
+            partialBoundedContextsData,
+          );
+        }
+      }
+    }
+    return boundedContextsData;
   }
 }
+
+const mergeBoundedContextData = (
+  mainBoundedContextData: TBoundedContexts,
+  newBoundedContextData: TBoundedContexts,
+): TBoundedContexts => {
+  const result = { ...mainBoundedContextData };
+  for (const [boundedContextName, boundedContextData] of Object.entries(newBoundedContextData)) {
+    for (const [classType, classTypeData] of Object.entries(boundedContextData)) {
+      for (const [className, classData] of Object.entries(classTypeData)) {
+        if (result[boundedContextName] && result[boundedContextName][classType]) {
+          result[boundedContextName][classType][className] = classData;
+        } else if (result[boundedContextName]) {
+          result[boundedContextName][classType] = { [className]: classData };
+        } else {
+          result[boundedContextName] = { [classType]: { [className]: classData } };
+        }
+      }
+    }
+  }
+  return result;
+};
