765207aa7f995f590ecba363a51291d319171ce7
Vasilis Danias <vasilis@bitloops.com>
2022-10-25 08:56:08 -0700

    [examples] Changed how DomainErrors are imported

--- examples/todo/ts-target/src/bounded-contexts/todo/todo/domain/TitleVO.ts
+++ examples/todo/ts-target/src/bounded-contexts/todo/todo/domain/TitleVO.ts
@@ -1,5 +1,5 @@
 import { Domain, Either, ok, fail } from '@bitloops/bl-boilerplate-core';
-import { DomainErrors } from './DomainErrors';
+import { DomainErrors } from './errors';
 import { TitleOutOfBoundsRule } from './Rules';
 
 interface TitleProps {
@@ -15,7 +15,7 @@ export class TitleVO extends Domain.ValueObject<TitleProps> {
     super(props);
   }
 
-  public static create(props: TitleProps): Either<TitleVO, DomainErrors.TitleOutOfBoundsError> {
+  public static create(props: TitleProps): Either<TitleVO, DomainErrors.TitleOutOfBounds> {
     const res = Domain.applyRules([new TitleOutOfBoundsRule(props.title)]);
     if (res) return fail(res);
     return ok(new TitleVO(props));
