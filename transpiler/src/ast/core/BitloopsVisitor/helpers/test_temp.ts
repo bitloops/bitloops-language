46e39e4a07403c34f025630fac78d085c9a217f9
Markos Girgis <markos@bitloops.com>
2022-10-27 09:55:08 +0300

    Fixes for demo setup (#45)

--- transpiler/src/ast/core/BitloopsVisitor/helpers/test_temp.ts
+++ transpiler/src/ast/core/BitloopsVisitor/helpers/test_temp.ts
@@ -1,64 +1,39 @@
 import {
-  BitloopsIntermediateASTParser,
-  BitloopsParser,
+  BitloopsIntermediateSetupASTParser,
   BitloopsParserError,
+  BitloopsSetupParser,
 } from '../../../../index.js';
 
 const blString = `
-Rule TitleOutOfBoundsRule(title: string) throws DomainErrors.TitleOutOfBoundsError {
-  isBrokenIf(title > 150 OR title < 4);
+RESTServer({
+  server: REST.Fastify,
+  port: Env(FASTIFY_PORT, 5001),
+  apiPrefix: '/api',
+  corsOptions: { origin: '*' },
+}) {
+  '/todo': todoRESTRouter;
 }
-
-Props TitleProps {
-  string title;
-}
-
-ValueObject TitleVO {
-  constructor(props: TitleProps): (OK(TitleVO), Errors(DomainErrors.TitleOutOfBoundsError)) {
-    applyRules(TitleOutOfBoundsRule(props.title));
-  }
-}
-
-// DomainError TitleOutOfBoundsError(title: string)  {
-//   message: \`Title \${title} is out of range\`,
-//   errorId: 'fe53432-8ef7-42349-ab67-cb83d1d7772fe',
-// }
-
-Props TodoProps {
-  optional UUIDv4 id;
-  TitleVO title;
-  bool completed;
-}
-
-Root Entity TodoEntity {
-  constructor(props: TodoProps): (OK(TodoEntity), Errors()) {}
-
-  complete(): (OK(void), Errors()) {
-    this.completed = true;
-  }
-
-  uncomplete(): (OK(void), Errors()) {
-    this.completed = false;
-  }
-
-  updateTitle(title: TitleVO):  (OK(void), Errors()) {
-    this.title = title;
-  }
-}
-
   `;
 
-const parser = new BitloopsParser();
-const initialModelOutput = parser.parse([
-  {
-    boundedContext: 'a',
-    module: 'b',
-    fileId: 'testFile.bl',
-    fileContents: blString,
-  },
-]);
-const intermediateParser = new BitloopsIntermediateASTParser();
+// const parser = new BitloopsParser();
+// const initialModelOutput = parser.parse([
+//   {
+//     boundedContext: 'a',
+//     module: 'b',
+//     fileId: 'testFile.bl',
+//     fileContents: blString,
+//   },
+// ]);
+// const intermediateParser = new BitloopsIntermediateASTParser();
+// if (!(initialModelOutput instanceof BitloopsParserError)) {
+//   const result = intermediateParser.parse(initialModelOutput);
+//   console.log('result:', JSON.stringify(result));
+// }
+
+const parser = new BitloopsSetupParser();
+const initialModelOutput = parser.parse(blString);
+const intermediateParser = new BitloopsIntermediateSetupASTParser();
 if (!(initialModelOutput instanceof BitloopsParserError)) {
-  const result = intermediateParser.parse(initialModelOutput);
+  const result = intermediateParser.parse(initialModelOutput as any);
   console.log('result:', JSON.stringify(result));
 }
