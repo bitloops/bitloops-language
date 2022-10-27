f0bc8d863b04787606f5e270a8c85522bd4d900f
Spiros Grigoratos <57859686+Traigor@users.noreply.github.com>
2022-10-27 15:24:08 +0300

    fixed switch: caseValue to be ExpressionValues (#52)

--- transpiler/src/ast/core/BitloopsVisitor/helpers/switchStatement.ts
+++ transpiler/src/ast/core/BitloopsVisitor/helpers/switchStatement.ts
@@ -59,11 +59,11 @@ export const caseClauseVisitor = (
   thisVisitor: BitloopsVisitor,
   ctx: BitloopsParser.CaseClauseContext,
 ): TRegularCase => {
-  const caseValue = ctx.expression().getText();
+  const caseValue = thisVisitor.visit(ctx.expression());
   const caseStatement = thisVisitor.visit(ctx.statementList());
   return {
     statements: caseStatement.statements[0].statements,
-    caseValue: caseValue,
+    caseValue: caseValue.expression,
   };
 };
 
