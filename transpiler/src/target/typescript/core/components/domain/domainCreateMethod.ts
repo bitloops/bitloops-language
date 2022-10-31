f010551d93f8b4a559c5732470db7df29a6625a8
ellik95 <ellikotoula@gmail.com>
2022-10-31 18:52:11 +0200

    removed statementsWithoutThis from entity and vo

--- transpiler/src/target/typescript/core/components/domain/domainCreateMethod.ts
+++ transpiler/src/target/typescript/core/components/domain/domainCreateMethod.ts
@@ -17,46 +17,18 @@
  *
  *  For further information you can contact legal(at)bitloops.com.
  */
-import {
-  TDomainCreateMethod,
-  TStatement,
-  TTargetDependenciesTypeScript,
-} from '../../../../../types.js';
+import { TDomainCreateMethod, TTargetDependenciesTypeScript } from '../../../../../types.js';
 import { BitloopsTypesMapping, ClassTypes } from '../../../../../helpers/mappings.js';
 import { modelToTargetLanguage } from '../../modelToTargetLanguage.js';
 import { internalConstructor } from './index.js';
 
-const THIS_STATEMENT_DECLARATION = 'thisDeclaration';
-
 export const domainCreate = (create: TDomainCreateMethod): TTargetDependenciesTypeScript => {
   const { parameterDependency, returnType, statements } = create;
-
-  const statementsResult = {
-    thisStatements: [],
-    restStatements: [],
-  };
-
-  for (const statement of statements) {
-    if (isStatementThisDeclaration(statement)) {
-      statementsResult.thisStatements.push(statement);
-    } else {
-      statementsResult.restStatements.push(statement);
-    }
-  }
 
   const propsName = create.parameterDependency.type;
   const returnOkType = returnType.ok;
 
-  const producedConstructor = internalConstructor(
-    propsName,
-    statementsResult.thisStatements,
-    ClassTypes.ValueObjects,
-  );
-
-  let statementsModel = modelToTargetLanguage({
-    type: BitloopsTypesMapping.TStatementsWithoutThis,
-    value: statementsResult.restStatements,
-  });
+  const producedConstructor = internalConstructor(propsName, statements, ClassTypes.ValueObjects);
 
   const parameterModel = modelToTargetLanguage({
     type: BitloopsTypesMapping.TParameterDependency,
@@ -68,40 +40,15 @@ export const domainCreate = (create: TDomainCreateMethod): TTargetDependenciesTy
     value: returnType,
   });
 
-  const statementValues = statements.map((statement) => statement.valueOf());
-  const hasReturnStatements: boolean =
-    statementValues.filter(
-      (statement) => Object.keys(statement)[0] === BitloopsTypesMapping.TReturnStatement,
-    ).length === 0;
-  if (hasReturnStatements || statements.length === 0) {
-    statementsModel = {
-      output: statementsModel.output.concat(`return ok(new ${returnOkType}(props));`),
-      dependencies: statementsModel.dependencies,
-    };
-  }
-  const ToLanguageMapping = (
-    returnType: string,
-    parameterString: string,
-    methodStatements: string,
-  ): string => {
-    return `${producedConstructor.output} public static create(${parameterString}): ${returnType} { ${methodStatements} }`;
-  };
-  const result = ToLanguageMapping(
-    returnTypeModel.output,
-    parameterModel.output,
-    statementsModel.output,
-  );
+  const returnOkCreateStatement = `return ok(new ${returnOkType}(props));`;
+  const result = `${producedConstructor.output} public static create(${parameterModel.output}): ${returnTypeModel.output} { ${returnOkCreateStatement} }`;
+
   return {
     output: result,
     dependencies: [
-      ...statementsModel.dependencies,
       ...parameterModel.dependencies,
       ...producedConstructor.dependencies,
       ...returnTypeModel.dependencies,
     ],
   };
-};
-
-const isStatementThisDeclaration = (statement: TStatement): boolean => {
-  return Object.keys(statement)[0] === THIS_STATEMENT_DECLARATION;
 };
