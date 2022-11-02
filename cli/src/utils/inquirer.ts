911aae068b51233b24cf5fd458bdd4cfded807fc
Vasilis Danias <danias@users.noreply.github.com>
2022-11-02 13:52:25 +0700

    Update inquirer.ts
    Improved formatting

--- cli/src/utils/inquirer.ts
+++ cli/src/utils/inquirer.ts
@@ -1,8 +1,9 @@
+import inquirer, { Question } from 'inquirer';
 import fz from 'inquirer-fuzzy-path';
+import path from 'path';
 
 import { TAB, redColor } from '../utils/oraUtils.js';
-import inquirer, { Question } from 'inquirer';
-import path from 'path';
+
 export const inquirerFuzzy = async (q: Question, source: any): Promise<string> => {
   inquirer.registerPrompt('fuzzypath', fz);
   const answer = await inquirer.prompt(
@@ -23,6 +24,7 @@ export const inquirerFuzzy = async (q: Question, source: any): Promise<string> =
   const inputPath = answer[q.name];
   return path.normalize(`${process.cwd()}/${inputPath}`);
 };
+
 export const printError = (message: string): void => {
   console.log(TAB + '❌ ' + redColor(message));
 };
