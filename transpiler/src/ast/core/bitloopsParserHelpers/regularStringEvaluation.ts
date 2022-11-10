e18d1b4f2559c0a42dbb8fd797b30b4f0b63c680
Markos Girgis <mg.markosgirgis@gmail.com>
2022-11-10 13:25:45 +0200

    Remove old parser files

--- transpiler/src/ast/core/bitloopsParserHelpers/regularStringEvaluation.ts
+++
@@ -1,3 +0,0 @@
-export const regularStringEvaluation = (value: any): any => {
-  return { type: 'string', value: value.substring(1, value.length - 1) };
-};
--- transpiler/src/ast/core/bitloopsParserHelpers/statement.ts
+++
@@ -1,5 +0,0 @@
-import { getBitloopsModel, TSourceElementContext } from '../BitloopsParser.js';
-
-export const statement = (children: any[], contextSourceElement?: TSourceElementContext) => {
-  return getBitloopsModel(children[0], contextSourceElement);
-};
