e23910eb8f12da0950d5ffed16d07695f2b8a494
frostedpenguin <afrostedpenguin@gmail.com>
2022-11-23 15:31:37 +0200

    init

--- transpiler/src/parser/core/grammar/AntlerParser.d.ts
+++ transpiler/src/parser/core/grammar/AntlerParser.d.ts
@@ -1,12 +1,49 @@
 export class AntlerParser extends BitloopsParserListener {
   constructor(string: any);
   parser: BitloopsParser;
-  _tree: BitloopsParser.ProgramContext;
-  getBitloopsModel: any;
+  _tree: {
+    parser: any;
+    ruleIndex: number;
+    sourceElement: (i: any) => any;
+    enterRule(listener: any): void;
+    exitRule(listener: any): void;
+    accept(visitor: any): any;
+  };
   get test(): any;
-  get tree(): BitloopsParser.ProgramContext;
-  get bitloopsTree(): any;
-  enterProgram(ctx: any): void;
+  get tree(): {
+    parser: any;
+    ruleIndex: number;
+    sourceElement: (i: any) => any;
+    enterRule(listener: any): void;
+    exitRule(listener: any): void;
+    accept(visitor: any): any;
+  };
+  get bitloopsTree():
+    | {
+        type: any;
+        value: any;
+        numOfChildren?: undefined;
+        children?: undefined;
+      }
+    | {
+        type: any;
+        value: any;
+        numOfChildren: any;
+        children: any;
+      };
+  _bitloopsTree:
+    | {
+        type: any;
+        value: any;
+        numOfChildren?: undefined;
+        children?: undefined;
+      }
+    | {
+        type: any;
+        value: any;
+        numOfChildren: any;
+        children: any;
+      };
 }
 import BitloopsParserListener from './BitloopsParserListener.js';
 import BitloopsParser from './BitloopsParser.js';
