540ef21dbdc49f01c51c3952586b042606672494
Markos Girgis <mg.markosgirgis@gmail.com>
2022-11-28 11:43:26 +0200

    add evaluation field nodes

--- transpiler/src/refactoring-arch/intermediate-ast/nodes/Expression/Evaluation/EvaluationFieldList/EvaluationFieldNode.ts
+++ transpiler/src/refactoring-arch/intermediate-ast/nodes/Expression/Evaluation/EvaluationFieldList/EvaluationFieldNode.ts
@@ -4,9 +4,9 @@ import { IntermediateASTNode } from '../../../IntermediateASTNode.js';
 const NAME = 'evaluationField';
 
 // THis type will change to something like = { evaluationField: { name: string } & TExpression }
-// export type TEvaluationFields = ({ name: string } & TExpression)[];
+// export type TEvaluationField = ({ name: string } & TExpression)[];
 
-export class FieldNode extends IntermediateASTNode {
+export class EvaluationFieldNode extends IntermediateASTNode {
   constructor(lines?: string) {
     super(BitloopsTypesMapping.TEvaluationField, { lines }, NAME);
   }
