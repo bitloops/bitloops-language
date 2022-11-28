0c915db6537977f4461d6ca5fd4f33363b3e022f
Markos Girgis <mg.markosgirgis@gmail.com>
2022-11-28 15:13:04 +0200

    add entity evaluation

--- transpiler/src/refactoring-arch/intermediate-ast/nodes/Expression/Evaluation/EntityEvaluation.ts
+++ transpiler/src/refactoring-arch/intermediate-ast/nodes/Expression/Evaluation/EntityEvaluation.ts
@@ -2,11 +2,11 @@ import { BitloopsTypesMapping } from '../../../../../helpers/mappings.js';
 import { TNodeMetadata } from '../../IntermediateASTNode.js';
 import { EvaluationNode } from './EvaluationNode.js';
 
-const NAME = 'valueObject';
-export class ValueObjectEvaluationNode extends EvaluationNode {
+const NAME = 'entity';
+export class EntityEvaluationNode extends EvaluationNode {
   constructor(metadata?: TNodeMetadata) {
     super(metadata);
-    this.nodeType = BitloopsTypesMapping.TValueObjectEvaluation;
+    this.nodeType = BitloopsTypesMapping.TEntityEvaluation;
     this.classNodeName = NAME;
   }
 }
