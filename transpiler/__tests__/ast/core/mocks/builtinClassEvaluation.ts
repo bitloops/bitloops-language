b82ea1514fe20047b55d96a1dde8957c970926de
frostedpenguin <baltinon2@gmail.com>
2022-12-12 16:46:33 +0200

    last fixes 🧰

--- transpiler/__tests__/ast/core/mocks/builtinClassEvaluation.ts
+++ transpiler/__tests__/ast/core/mocks/builtinClassEvaluation.ts
@@ -7,12 +7,5 @@ export const validBuiltinClassEvaluations = [
     inputBLString: 'JestTestBuiltInClass {  UUIDv4(id) }',
     builtInIdentifier: 'UUIDv4',
     argumentList: new ArgumentListBuilderDirector().buildArgumentList(['id']),
-    // expected: new EvaluationBuilderDirector().buildBuiltInClassEvaluation()
-    // expected: {
-    //   builtInClass: {
-    //     className: 'UUIDv4',
-    //     argumentList: [{ argument: { expression: { identifier: 'id' } } }],
-    //   },
-    // },
   },
 ];
