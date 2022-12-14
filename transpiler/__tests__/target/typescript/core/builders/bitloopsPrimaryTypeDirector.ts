e192389f9248583dfae8f05d4ec2190a20458560
Markos Girgis <mg.markosgirgis@gmail.com>
2022-12-14 18:51:47 +0200

    fix const declaration target

--- transpiler/__tests__/target/typescript/core/builders/bitloopsPrimaryTypeDirector.ts
+++ transpiler/__tests__/target/typescript/core/builders/bitloopsPrimaryTypeDirector.ts
@@ -1,8 +1,13 @@
 import { BitloopsIdentifierTypeBuilder } from '../../../../../src/ast/core/intermediate-ast/builders/BitloopsPrimaryType/BitloopsIdentifierTypeBuilder.js';
 import { BitloopsPrimaryTypeBuilder } from '../../../../../src/ast/core/intermediate-ast/builders/BitloopsPrimaryType/BitloopsPrimaryTypeBuilder.js';
+import { BuildInClassTypeBuilder } from '../../../../../src/ast/core/intermediate-ast/builders/BitloopsPrimaryType/BuildInClassTypeBuilder.js';
 import { PrimitiveTypeBuilder } from '../../../../../src/ast/core/intermediate-ast/builders/BitloopsPrimaryType/PrimitiveTypeBuilder.js';
 import { BitloopsPrimaryTypeNode } from '../../../../../src/ast/core/intermediate-ast/nodes/BitloopsPrimaryType/BitloopsPrimaryTypeNode.js';
-import { TBitloopsIdentifier, TBitloopsPrimitives } from '../../../../../src/types.js';
+import {
+  TBitloopsBuiltInClasses,
+  TBitloopsIdentifier,
+  TBitloopsPrimitives,
+} from '../../../../../src/types.js';
 
 export class BitloopsPrimaryTypeDirector {
   // buildArrayPrimaryType(type: TBitloopsPrimitives): BitloopsPrimaryTypeNode {
@@ -26,5 +31,8 @@ export class BitloopsPrimaryTypeDirector {
     return new BitloopsPrimaryTypeBuilder().withPrimaryType(bitloopsIdentifierTypeNode).build();
   }
 
-  // buildBuiltinClassPrimaryType(type: TBitloopsBuiltInClasses): BitloopsPrimaryTypeNode {}
+  buildBuiltinClassPrimaryType(type: TBitloopsBuiltInClasses): BitloopsPrimaryTypeNode {
+    const buildInClassTypeNode = new BuildInClassTypeBuilder().withType(type).build();
+    return new BitloopsPrimaryTypeBuilder().withPrimaryType(buildInClassTypeNode).build();
+  }
 }
--- transpiler/__tests__/target/typescript/core/builders/statement/constDeclaration.ts
+++ transpiler/__tests__/target/typescript/core/builders/statement/constDeclaration.ts
@@ -1,18 +1,27 @@
 import { EvaluationFieldListNodeBuilder } from '../../../../../../src/ast/core/intermediate-ast/builders/expressions/evaluation/EvaluationFieldList/EvaluationFieldListNodeBuilder.js';
 import { IdentifierNodeBuilder } from '../../../../../../src/ast/core/intermediate-ast/builders/identifier/IdentifierBuilder.js';
 import { ConstDeclarationNodeBuilder } from '../../../../../../src/ast/core/intermediate-ast/builders/statements/constDeclaration.js';
+import { EvaluationFieldNode } from '../../../../../../src/ast/core/intermediate-ast/nodes/Expression/Evaluation/EvaluationFieldList/EvaluationFieldNode.js';
 import { ExpressionNode } from '../../../../../../src/ast/core/intermediate-ast/nodes/Expression/ExpressionNode.js';
 import { ConstDeclarationNode } from '../../../../../../src/ast/core/intermediate-ast/nodes/statements/ConstDeclarationNode.js';
+import { TypeAnnotationNode } from '../../../../../../src/ast/core/intermediate-ast/nodes/TypeAnnotationNode.js';
 import { ArgumentListDirector } from '../argumentList.js';
 import { EvaluationBuilderDirector } from '../evaluation.js';
 import { ExpressionBuilderDirector } from '../expression.js';
 
 export class ConstDeclarationBuilderDirector {
-  buildConstDeclaration(identifier: string, expression: ExpressionNode): ConstDeclarationNode {
-    return new ConstDeclarationNodeBuilder()
+  buildConstDeclaration(
+    identifier: string,
+    expression: ExpressionNode,
+    typeAnnotation?: TypeAnnotationNode,
+  ): ConstDeclarationNode {
+    const node = new ConstDeclarationNodeBuilder()
       .withIdentifier(new IdentifierNodeBuilder().withName(identifier).build())
-      .withExpression(expression)
-      .build();
+      .withExpression(expression);
+    if (typeAnnotation) {
+      node.withTypeAnnotation(typeAnnotation);
+    }
+    return node.build();
   }
   /**
    *  const {identifier} = [await]? this.useCase.execute();
@@ -72,6 +81,48 @@ export class ConstDeclarationBuilderDirector {
           new EvaluationFieldListNodeBuilder().withEvaluationFields([]).build(),
         ),
       ),
+    );
+  }
+
+  buildStructEvaluationConstDeclaration(
+    identifier: string,
+    structIdentifier: string,
+    evaluationFieldNodes: EvaluationFieldNode[],
+    typeAnnotation?: TypeAnnotationNode,
+  ): ConstDeclarationNode {
+    return this.buildConstDeclaration(
+      identifier,
+      new ExpressionBuilderDirector().buildEvaluationExpression(
+        new EvaluationBuilderDirector().buildStructEvaluation(
+          structIdentifier,
+          evaluationFieldNodes,
+        ),
+      ),
+      typeAnnotation,
+    );
+  }
+
+  buildIntegerExpressionConstDeclaration(
+    identifier: string,
+    value: number,
+    typeAnnotation?: TypeAnnotationNode,
+  ): ConstDeclarationNode {
+    return this.buildConstDeclaration(
+      identifier,
+      new ExpressionBuilderDirector().buildInt32LiteralExpression(value),
+      typeAnnotation,
+    );
+  }
+
+  buildStringExpressionConstDeclaration(
+    identifier: string,
+    value: string,
+    typeAnnotation?: TypeAnnotationNode,
+  ): ConstDeclarationNode {
+    return this.buildConstDeclaration(
+      identifier,
+      new ExpressionBuilderDirector().buildStringLiteralExpression(value),
+      typeAnnotation,
     );
   }
 }
