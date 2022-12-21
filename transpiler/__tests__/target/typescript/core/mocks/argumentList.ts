fe12a99032c8a479830918ddaee048529a533a8b
Markos Girgis <mg.markosgirgis@gmail.com>
2022-12-21 10:51:08 +0200

    fix argument List

--- transpiler/__tests__/target/typescript/core/mocks/argumentList.ts
+++ transpiler/__tests__/target/typescript/core/mocks/argumentList.ts
@@ -1,5 +1,7 @@
 import { ArgumentListNode } from '../../../../../src/ast/core/intermediate-ast/nodes/ArgumentList/ArgumentListNode.js';
+import { ArgumentDirector } from '../builders/argument.js';
 import { ArgumentListDirector } from '../builders/argumentList.js';
+import { ExpressionBuilderDirector } from '../builders/expression.js';
 
 type TestCase = {
   description: string;
@@ -7,10 +9,6 @@ type TestCase = {
   output: string;
 };
 
-// | [{"value": "helloWorld", "type": "string" }]                                                                            | ('helloWorld')         |
-// | [{"value": "42", "type": "int32" }]                                                                                     | (42)                   |
-// | [{"value": "person", "type": "variable" }]                                                                              | (person)               |
-// | [{"value": "person", "type": "variable" }, {"value": "UncleBob", "type": "string" }]                                    | (person,'UncleBob')    |
 // | [{"value": "string", "type": "string" }, {"value": "42", "type": "int32" }, {"value": "children", "type": "variable" }] | ('string',42,children) |
 export const VALID_ARGUMENT_LIST_TEST_CASES: Array<TestCase> = [
   {
@@ -27,5 +25,32 @@ export const VALID_ARGUMENT_LIST_TEST_CASES: Array<TestCase> = [
     description: 'a variable argument',
     argumentList: new ArgumentListDirector().buildArgumentListWithIdentifierExpression('person'),
     output: '(person)',
+  },
+  {
+    description: 'a string and an identifier argument',
+    argumentList: new ArgumentListDirector().buildArgumentListWithArgs([
+      new ArgumentDirector().buildArgument(
+        new ExpressionBuilderDirector().buildStringLiteralExpression('UncleBob'),
+      ),
+      new ArgumentDirector().buildArgument(
+        new ExpressionBuilderDirector().buildIdentifierExpression('person'),
+      ),
+    ]),
+    output: "('UncleBob',person)",
+  },
+  {
+    description: 'a string, a number argument and a variable argument',
+    argumentList: new ArgumentListDirector().buildArgumentListWithArgs([
+      new ArgumentDirector().buildArgument(
+        new ExpressionBuilderDirector().buildStringLiteralExpression('UncleBob'),
+      ),
+      new ArgumentDirector().buildArgument(
+        new ExpressionBuilderDirector().buildInt32LiteralExpression(42),
+      ),
+      new ArgumentDirector().buildArgument(
+        new ExpressionBuilderDirector().buildIdentifierExpression('person'),
+      ),
+    ]),
+    output: "('UncleBob',42,person)",
   },
 ];
