30556f0fa459173e17e1c03f85b0e4865d6fb8fe
Markos Girgis <mg.markosgirgis@gmail.com>
2022-12-21 11:17:19 +0200

    add use-case dot-value check

--- transpiler/src/target/ast/node-transformers/use-case.ts
+++ transpiler/src/target/ast/node-transformers/use-case.ts
@@ -1,4 +1,5 @@
 import { IdentifierExpressionNode } from '../../../ast/core/intermediate-ast/nodes/Expression/IdentifierExpression.js';
+import { MemberDotExpressionNode } from '../../../ast/core/intermediate-ast/nodes/Expression/MemberDot/MemberDotExpression.js';
 import { MethodCallExpressionNode } from '../../../ast/core/intermediate-ast/nodes/Expression/MethodCallExpression.js';
 import { UseCaseNode } from '../../../ast/core/intermediate-ast/nodes/UseCase/UseCaseNode.js';
 import { NodeModelToTargetASTTransformer } from './index.js';
@@ -38,8 +39,35 @@ export class UseCaseNodeTSTransformer extends NodeModelToTargetASTTransformer<Us
   private updateIdentifierNodes(identifierExpressionNodes: IdentifierExpressionNode[]): void {
     identifierExpressionNodes.forEach(
       // Update only if i don't have a grandparent, or my grandparent is not an expression/member dot
-      (node) => (node.identifierName = this.appendDotValue(node.identifierName)),
+      (node) => {
+        if (this.identifierIsUsedByMemberDotExpression(node)) {
+          return;
+        }
+        node.identifierName = this.appendDotValue(node.identifierName);
+      },
     );
+  }
+
+  /**
+   *  If its the left part of a member dot expression, then its grandParent is a member dot expression
+   * if its the right part of a member dot expression, then its parent is a member dot expression
+   */
+  private identifierIsUsedByMemberDotExpression(identifier: IdentifierExpressionNode): boolean {
+    const parent = identifier.getParent();
+    if (!parent) {
+      return false;
+    }
+    const identifierIsRightPartOfMemberDotExpression = parent instanceof MemberDotExpressionNode;
+    if (identifierIsRightPartOfMemberDotExpression) {
+      return true;
+    }
+    const grandParent = parent.getParent();
+    if (!grandParent) {
+      return false;
+    }
+    const identifierIsLeftPartOfMemberDotExpression =
+      grandParent instanceof MemberDotExpressionNode;
+    return identifierIsLeftPartOfMemberDotExpression;
   }
 
   private prependAwaitToDependencies(dependencies: string[]): void {
