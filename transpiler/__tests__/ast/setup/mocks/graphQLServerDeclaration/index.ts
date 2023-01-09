13821b953e5adf723e26ae969d7ed48679378672
Spiros Grigoratos <spgrigoratos@gmail.com>
2023-01-09 15:04:19 +0200

    completed grphQLServer

--- transpiler/__tests__/ast/setup/mocks/graphQLServerDeclaration/index.ts
+++ transpiler/__tests__/ast/setup/mocks/graphQLServerDeclaration/index.ts
@@ -10,7 +10,7 @@ export const VALID_GRAPHQL_SERVER_CASES = [
     inputBLString: FileUtil.readFileString(
       'transpiler/__tests__/ast/setup/mocks/graphqlServerDeclaration/graphQLServerWithSingleController.bl',
     ),
-    description: 'Valid graphQL server',
+    description: 'Valid GraphQL server with single controller',
     fileId: 'testFile.bl',
     graphQLServer: new GraphQLServerDeclarationBuilder()
       .withOptions({
@@ -30,6 +30,93 @@ export const VALID_GRAPHQL_SERVER_CASES = [
           .withClassName('HelloWorldController')
           .withBoundedContextModule({ boundedContextName: 'Demo', moduleName: 'Hello World' })
           .withArguments({ argumentList: [] })
+          .build(),
+      ])
+      .build(),
+  },
+  {
+    inputBLString: FileUtil.readFileString(
+      'transpiler/__tests__/ast/setup/mocks/graphqlServerDeclaration/graphQLServerWithMultipleControllers.bl',
+    ),
+    description: 'Valid GraphQL server with multiple controllers',
+    fileId: 'testFile.bl',
+    graphQLServer: new GraphQLServerDeclarationBuilder()
+      .withOptions({
+        fields: [
+          new EvaluationFieldBuilderDirector().buildEvaluationField(
+            'port',
+            new ExpressionBuilderDirector().buildLogicalOrExpression(
+              new ExpressionBuilderDirector().buildEnvVariableExpression('env.APOLLO_SERVER'),
+              new ExpressionBuilderDirector().buildInt32LiteralExpression(5001),
+            ),
+          ),
+        ],
+      })
+      .withResolvers([
+        new GraphQLControllerResolverBuilder()
+          .withInstanceName('HelloWorldController')
+          .withClassName('HelloWorldController')
+          .withBoundedContextModule({ boundedContextName: 'Demo', moduleName: 'Hello World' })
+          .withArguments({ argumentList: [] })
+          .build(),
+        new GraphQLControllerResolverBuilder()
+          .withInstanceName('HelloWorld2Controller')
+          .withClassName('HelloWorld2Controller')
+          .withBoundedContextModule({ boundedContextName: 'Demo', moduleName: 'Bye World' })
+          .withArguments({ argumentList: [] })
+          .build(),
+      ])
+      .build(),
+  },
+  {
+    inputBLString: FileUtil.readFileString(
+      'transpiler/__tests__/ast/setup/mocks/graphqlServerDeclaration/graphQLServerWithDependencies.bl',
+    ),
+    description: 'Valid GraphQL server with dependencies',
+    fileId: 'testFile.bl',
+    graphQLServer: new GraphQLServerDeclarationBuilder()
+      .withOptions({
+        fields: [
+          new EvaluationFieldBuilderDirector().buildEvaluationField(
+            'port',
+            new ExpressionBuilderDirector().buildLogicalOrExpression(
+              new ExpressionBuilderDirector().buildEnvVariableExpression('env.APOLLO_SERVER'),
+              new ExpressionBuilderDirector().buildInt32LiteralExpression(5001),
+            ),
+          ),
+        ],
+      })
+      .withResolvers([
+        new GraphQLControllerResolverBuilder()
+          .withInstanceName('HelloWorldController')
+          .withClassName('HelloWorldController')
+          .withBoundedContextModule({ boundedContextName: 'Demo', moduleName: 'Hello World' })
+          .withArguments({
+            argumentList: [
+              {
+                argument: new ExpressionBuilderDirector().buildIdentifierExpression(
+                  'useCase1Dependency',
+                ),
+              },
+            ],
+          })
+          .build(),
+        new GraphQLControllerResolverBuilder()
+          .withInstanceName('HelloWorld2Controller')
+          .withClassName('HelloWorld2Controller')
+          .withBoundedContextModule({ boundedContextName: 'Demo', moduleName: 'Bye World' })
+          .withArguments({
+            argumentList: [
+              {
+                argument: new ExpressionBuilderDirector().buildIdentifierExpression(
+                  'useCase2Dependency',
+                ),
+              },
+              {
+                argument: new ExpressionBuilderDirector().buildIdentifierExpression('usersRepo'),
+              },
+            ],
+          })
           .build(),
       ])
       .build(),
