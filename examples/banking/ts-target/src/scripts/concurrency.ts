fede69cd949f53fb3471e1e0bb38b845cd8df118
Markos Girgis <mg.markosgirgis@gmail.com>
2023-02-06 16:45:40 +0200

    add script

--- examples/banking/ts-target/src/scripts/concurrency.ts
+++ examples/banking/ts-target/src/scripts/concurrency.ts
@@ -5,7 +5,8 @@ import { Mongo } from '@bitloops/bl-boilerplate-infra-mongo';
 // Connection URL
 // const url = "mongodb+srv://<username>:<password>@clustername.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 
-const url = 'mongodb://localhost:27021';
+// const url = 'mongodb://localhost:27021';
+const url = 'mongodb://localhost:30002/?directConnection=true&replicaSet=my-replica-set';
 const client = new Mongo.Client(url);
 
 // Database Name
@@ -14,10 +15,7 @@ const dbName = 'test_banking';
 const id = '85c5d65a-2c7e-4a9c-ad31-e367bb3c4f95';
 const account: any = {
   _id: id,
-  balance: {
-    currency: 'EUR',
-    amount: 20,
-  },
+  amount: 20,
 };
 
 async function main() {
@@ -32,88 +30,13 @@ async function main() {
   const insertResult = await collection.insertOne(account);
   console.log('Inserted documents =>', insertResult);
 
-  // Increases by 10
-  const sessionOne = client.startSession();
-  // Decreases by 10
-  const sessionTwo = client.startSession();
+  const promises = [];
+  promises.push(concurrencyClient((n) => n + 10));
+  promises.push(concurrencyClient((n) => n - 10));
+  promises.push(concurrencyClient((n) => n + 10));
+  promises.push(concurrencyClient((n) => n - 10));
 
-  const transactionOptions: Mongo.TransactionOptions = {
-    // readConcern: { level: 'snapshot' },
-    // writeConcern: { w: 'majority' },
-    // readPreference: 'primary',
-  };
-  try {
-    sessionOne.startTransaction(transactionOptions);
-
-    sessionTwo.startTransaction(transactionOptions);
-
-    const accountOne = await collection.findOne(
-      {
-        _id: id,
-      },
-      {
-        session: sessionOne,
-      },
-    );
-    console.log('accountOne =>', accountOne);
-
-    const accountTwo = await collection.findOne(
-      {
-        _id: id,
-      },
-      {
-        session: sessionTwo,
-      },
-    );
-    console.log('accountTwo =>', accountTwo);
-
-    await collection.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          balance: {
-            currency: 'EUR',
-            amount: accountOne!.balance.amount + 10,
-          },
-        },
-      },
-      {
-        session: sessionOne,
-      },
-    );
-    console.log('Added 10 to accountOne =>');
-
-    await sessionOne.commitTransaction();
-
-    await collection.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          balance: {
-            currency: 'EUR',
-            amount: accountTwo!.balance.amount - 10,
-          },
-        },
-      },
-      {
-        session: sessionTwo,
-      },
-    );
-    console.log('Subtracted 10 from accountTwo =>');
-
-    await sessionTwo.commitTransaction();
-  } catch (e) {
-    console.log('Error =>', e);
-    await sessionOne.abortTransaction();
-    await sessionTwo.abortTransaction();
-  } finally {
-    await sessionOne.endSession();
-    await sessionTwo.endSession();
-  }
+  await Promise.all(promises);
 
   const accountFindResult = await collection.find({}).toArray();
   console.log('Found documents =>', accountFindResult);
@@ -152,28 +75,24 @@ async function concurrencyClient(op: (n: number) => number) {
     );
     console.log('account after read =>', account);
 
-    const amount = op(account!.balance.amount);
+    const amount = op(account!.amount);
     await collection.updateOne(
       {
         _id: id,
       },
       {
         $set: {
-          balance: {
-            currency: 'EUR',
-            amount,
-          },
+          amount,
         },
       },
       {
         session,
       },
     );
-    console.log('Added 10 to accountOne =>');
+    console.log('amount after update =>', amount);
 
     await session.commitTransaction();
 
-    console.log('Subtracted 10 from accountTwo =>');
     await session.commitTransaction();
   } catch (e) {
     console.log('Error =>', e);
