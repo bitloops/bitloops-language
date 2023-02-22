11d2597463b838e5e2508451f21cc8f625f079cc
Markos Girgis <mg.markosgirgis@gmail.com>
2023-02-22 14:40:13 +0200

    use prepend-await in controllers

--- transpiler/src/target/typescript/ast/node-transformers/controllers/index.ts
+++ transpiler/src/target/typescript/ast/node-transformers/controllers/index.ts
@@ -1,33 +1,30 @@
+import { IntermediateASTTree } from '../../../../../ast/core/intermediate-ast/IntermediateASTTree.js';
 import { ControllerNode } from '../../../../../ast/core/intermediate-ast/nodes/controllers/ControllerNode.js';
 import { GraphQLControllerNode } from '../../../../../ast/core/intermediate-ast/nodes/controllers/graphql/GraphQLControllerNode.js';
 import { RESTControllerNode } from '../../../../../ast/core/intermediate-ast/nodes/controllers/restController/RESTControllerNode.js';
-import { ExpressionNode } from '../../../../../ast/core/intermediate-ast/nodes/Expression/ExpressionNode.js';
-import { MethodCallExpressionNode } from '../../../../../ast/core/intermediate-ast/nodes/Expression/MethodCallExpression.js';
+// import { ExpressionNode } from '../../../../../ast/core/intermediate-ast/nodes/Expression/ExpressionNode.js';
+// import { MethodCallExpressionNode } from '../../../../../ast/core/intermediate-ast/nodes/Expression/MethodCallExpression.js';
+import { PrependAwaitNodeTSTransformer } from '../generic/prependAwait.js';
 import { NodeModelToTargetASTTransformer } from '../index.js';
 
 class BaseControllerNodeTSTransformer<
   T extends ControllerNode,
 > extends NodeModelToTargetASTTransformer<T> {
+  private prependAwaitTransformer: PrependAwaitNodeTSTransformer;
+
+  constructor(protected tree: IntermediateASTTree, protected node: T) {
+    super(tree, node);
+    this.prependAwaitTransformer = new PrependAwaitNodeTSTransformer(node, tree);
+  }
   run(): void {
-    this.transformAwait();
+    // this.transformAwait();
+    this.prependAwaitToAllDependencyCalls();
     this.transformDotValue();
   }
 
-  private transformAwait(): void {
-    const executeStatement = this.tree.getUseCaseExecuteStatementOf(this.node);
-    if (!executeStatement) {
-      return;
-    }
-
-    if (executeStatement.isExpressionNode()) {
-      return this.handleAwaitOfPlainMethodCall(executeStatement);
-    }
-
-    const expression = executeStatement.getExpressionValues();
-    if (!expression.isMethodCallExpression()) {
-      throw new Error('Method call expression not found');
-    }
-    this.prependAwaitToMethodCallNode(expression);
+  private prependAwaitToAllDependencyCalls(): void {
+    const extraDependencies = this.node.getExtraDependencies() ?? [];
+    this.prependAwaitTransformer.prependAwaitToAllDependencyCalls(extraDependencies);
   }
 
   private transformDotValue(): void {
@@ -48,19 +45,6 @@ class BaseControllerNodeTSTransformer<
       identifierValue,
       identifierWithAppendedDotValue,
     );
-  }
-
-  private handleAwaitOfPlainMethodCall(expression: ExpressionNode): void {
-    const methodCallExpression = expression.getChildren()[0] as ExpressionNode;
-    if (!methodCallExpression.isMethodCallExpression()) {
-      throw new Error('Method call expression not found');
-    }
-    this.prependAwaitToMethodCallNode(methodCallExpression);
-  }
-
-  private prependAwaitToMethodCallNode(node: MethodCallExpressionNode): void {
-    const thisNode = node.getThisNode();
-    thisNode.updateValue('await this');
   }
 
   private appendDotValue(str: string): string {
