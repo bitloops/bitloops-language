a138415efd0d1d2aa707242362e48fa41419f17d
giorgosnty <g.ntymenos@gmail.com>
2023-03-30 11:52:38 +0300

    published telemetry

---
+++ boilerplate/ts/infra/telemetry/src/index.ts
@@ -0,0 +1,8 @@
+import { AsyncLocalStorageService } from './async-local-storage.service';
+
+export { Traceable } from './traceable.decorator';
+export { CorrelationIdMiddleware } from './traces.middleware';
+export { TracingModule } from './tracing.module';
+
+// This is to be removed, and replaced with the AsyncLocalStorageService
+export const asyncLocalStorage = AsyncLocalStorageService.asyncLocalStorage;
