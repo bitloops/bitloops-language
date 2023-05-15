0e13a7e0ca7581d152e631935f4bc662ef3f9992
Markos Girgis <markos@bitloops.com>
2023-05-15 14:51:27 +0300

    update failing test-case

--- transpiler/src/test.ts
+++ transpiler/src/test.ts
@@ -1,18 +1,29 @@
 import { transpiler } from './index.js';
 
 const input = `
-CommandHandler ACommandHandler {
-    execute():  (OK(string), Errors()){
-
+CommandHandler ACommandHandler() {
+    execute(command: AddTodoCommand):  (OK(string), Errors()){
+        const title = TitleVO.create({ title: command.title });
     }
 }
 `;
+
+const input2 = `Command AddTodoCommand {
+    string title;
+}`;
 transpiler.bitloopsCodeToIntermediateModel({
   core: [
     {
       fileContents: input,
       fileName: 'test',
       fileId: 'test',
+      boundedContext: 'hello',
+      module: 'world',
+    },
+    {
+      fileContents: input2,
+      fileName: 'test2',
+      fileId: 'test2',
       boundedContext: 'hello',
       module: 'world',
     },
