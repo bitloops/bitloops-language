e3aeb0a188a107609ebaa12acae5dd75e74c59b2
Markos Girgis <markos@bitloops.com>
2023-06-12 13:07:59 +0300

    change todo folder structure

--- examples/todo/project/backend/src/lib/bounded-contexts/todo/todo/application/command-handlers/delete-todo.command-handler.ts
+++ examples/todo/project/backend/src/lib/bounded-contexts/todo/todo/application/command-handlers/delete-todo.command-handler.ts
@@ -7,40 +7,37 @@ import {
 } from '@bitloops/bl-boilerplate-core';
 import { Traceable } from '@bitloops/bl-boilerplate-infra-telemetry';
 import { ApplicationErrors } from '../errors/index';
-import { DomainErrors } from '../../domain/errors/index';
-import { UncompleteTodoCommand } from '../../commands/uncomplete-todo.command';
+import { DeleteTodoCommand } from '../../commands/delete-todo.command';
 import { Inject } from '@nestjs/common';
 import { TodoWriteRepoPortToken } from '../../constants';
 import { TodoWriteRepoPort } from '../../ports/todo-write.repo-port';
-export type UncompleteTodoCommandHandlerResponse = Either<
+export type DeleteTodoCommandHandlerResponse = Either<
   void,
-  | Application.Repo.Errors.Unexpected
-  | ApplicationErrors.TodoNotFoundError
-  | DomainErrors.TodoAlreadyUncompletedError
+  Application.Repo.Errors.Unexpected | ApplicationErrors.TodoNotFoundError
 >;
-export class UncompleteTodoCommandHandler
-  implements Application.ICommandHandler<UncompleteTodoCommand, void>
+export class DeleteTodoCommandHandler
+  implements Application.ICommandHandler<DeleteTodoCommand, void>
 {
   constructor(
     @Inject(TodoWriteRepoPortToken)
     private readonly todoRepo: TodoWriteRepoPort
   ) {}
   get command() {
-    return UncompleteTodoCommand;
+    return DeleteTodoCommand;
   }
   get boundedContext(): string {
     return 'todo';
   }
   @Traceable({
-    operation: 'UncompleteTodoCommandHandler',
+    operation: 'DeleteTodoCommandHandler',
     metrics: {
-      name: 'UncompleteTodoCommandHandler',
+      name: 'DeleteTodoCommandHandler',
       category: 'commandHandler',
     },
   })
   async execute(
-    command: UncompleteTodoCommand
-  ): Promise<UncompleteTodoCommandHandlerResponse> {
+    command: DeleteTodoCommand
+  ): Promise<DeleteTodoCommandHandlerResponse> {
     const todoId = new Domain.UUIDv4(command.id);
     const todoEntity = await this.todoRepo.getById(todoId);
     if (todoEntity.isFail()) {
@@ -49,13 +46,13 @@ export class UncompleteTodoCommandHandler
     if (!todoEntity.value) {
       return fail(new ApplicationErrors.TodoNotFoundError(command.id));
     }
-    const result_682966 = todoEntity.value.uncomplete();
-    if (result_682966.isFail()) {
-      return fail(result_682966.value);
+    const result_418023 = todoEntity.value.delete();
+    if (result_418023.isFail()) {
+      return fail(result_418023.value);
     }
-    const updateResult = await this.todoRepo.update(todoEntity.value);
-    if (updateResult.isFail()) {
-      return fail(updateResult.value);
+    const deleteResult = await this.todoRepo.delete(todoEntity.value);
+    if (deleteResult.isFail()) {
+      return fail(deleteResult.value);
     }
     return ok();
   }
--- examples/todo/project/backend/src/lib/bounded-contexts/todo/todo/application/command-handlers/modify-todo-title.command-handler.ts
+++ examples/todo/project/backend/src/lib/bounded-contexts/todo/todo/application/command-handlers/modify-todo-title.command-handler.ts
@@ -52,9 +52,9 @@ export class ModifyTodoTitleCommandHandler
     if (title.isFail()) {
       return fail(title.value);
     }
-    const result_599480 = todoFound.value.modifyTitle(title.value);
-    if (result_599480.isFail()) {
-      return fail(result_599480.value);
+    const result_917653 = todoFound.value.modifyTitle(title.value);
+    if (result_917653.isFail()) {
+      return fail(result_917653.value);
     }
     const updateResult = await this.todoRepo.update(todoFound.value);
     if (updateResult.isFail()) {
