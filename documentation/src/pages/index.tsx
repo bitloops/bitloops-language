0078c3fb69267324e8b5b02e1c6099a8415cbd49
Vasilis Danias <vasilis@bitloops.com>
2025-01-17 18:23:41 +0200

    Fixed broken link

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
